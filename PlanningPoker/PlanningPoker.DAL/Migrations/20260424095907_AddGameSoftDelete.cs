b4a08e283dea6380ac7dc45297cbb2494851299e
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2026-04-24 13:12:02 +0300

    feat: add soft delete game

---
+++ PlanningPoker/PlanningPoker.DAL/Migrations/20260424095907_AddGameSoftDelete.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanningPoker.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Name_CreatedBy",
+                table: "Games",
+                columns: new[] { "Name", "CreatedBy" },
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Games_Name_CreatedBy",
+                table: "Games");
+        }
+    }
+}
