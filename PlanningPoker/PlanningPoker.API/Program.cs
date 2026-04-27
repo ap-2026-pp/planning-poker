d288cc2140423a941c603e805deeda98164e0509
Solohub <nastia.sologub11113@gmail.com>
2026-04-27 11:12:48 +0300

    remove Identity roles

--- PlanningPoker/PlanningPoker.API/Program.cs
+++ PlanningPoker/PlanningPoker.API/Program.cs
@@ -133,7 +133,6 @@ static async Task SeedDatabaseAsync(WebApplication app)
 
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
-        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
         await DbInitializer.SeedDataAsync(context, userManager);
     }
     catch (Exception ex)
