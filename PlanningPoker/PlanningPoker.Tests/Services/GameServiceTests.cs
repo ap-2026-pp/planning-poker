c93aff9ffe19db1c6cb512c7a87325ffb1861c79
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2026-04-29 09:47:41 +0300

    test: add unit tests for game participant service

--- PlanningPoker/PlanningPoker.Tests/Services/GameServiceTests.cs
+++ PlanningPoker/PlanningPoker.Tests/Services/GameServiceTests.cs
@@ -6,7 +6,7 @@ using PlanningPoker.Domain.Interfaces.Repositories;
 using PlanningPoker.Domain.Interfaces.Services;
 using PlanningPoker.Domain.Models;
 
-namespace PlanningPoker.Tests.BLL;
+namespace PlanningPoker.Tests.Services;
 
 public class GameServiceTests
 {
