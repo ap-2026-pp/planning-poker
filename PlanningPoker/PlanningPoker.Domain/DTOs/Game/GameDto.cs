e1b0dea4eec34b6856ddf03c522776ba345721ae
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2026-05-01 12:45:38 +0300

    fix: add issues to game dto

--- PlanningPoker/PlanningPoker.Domain/DTOs/Game/GameDto.cs
+++ PlanningPoker/PlanningPoker.Domain/DTOs/Game/GameDto.cs
@@ -1,3 +1,4 @@
+using PlanningPoker.BLL.DTOs.Issue;
 using PlanningPoker.Domain.Models;
 
 namespace PlanningPoker.Domain.DTOs.Game;
@@ -14,5 +15,6 @@ public class GameDto
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public Guid? CreatedBy { get; set; }
-    public ICollection<GameParticipantDto> Participants { get; set; } = [];
+    public ICollection<GameParticipantDto>? Participants { get; set; }
+    public ICollection<IssueDto>? Issues { get; set; }
 }
